14aa0e680d867112c16aa0972d918c59db9dd66b
ShengJieSang <shengj.sang@icloud.com>
2022-05-03 17:52:15 +0800

    feat: 新增 1.2章 格式化输出
    1.2.1 调试
    1.2.2 显示
    1.2.3 测试实例:List
    1.2.4 格式化

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::fmt;
+use std::fmt::Formatter;
+
 fn main() {
     // chapter 1.0
     println!("Hello, world!");
@@ -23,8 +26,232 @@ fn main() {
     println!("{number:>width$}", number = 1, width = 6);
     println!("{number:0>width$}", number = 1, width = 6);
     println!("My name is {0}, {1} {0}", "Bond", "James");
+
+    // chapter 1.2.1
     #[allow(dead_code)]
     #[derive(Debug)]
     struct Structure(i32);
     println!("This struct `{:?}` won't print...", Structure(3));
+
+    println!("{:?} months in a year.", 12);
+    println!(
+        "{1:?} {0:?} is the {actor:?} name.",
+        "Slater",
+        "Christian",
+        actor = "actor's"
+    );
+
+    println!("Now {:?} will print!", Structure(3));
+
+    #[derive(Debug)]
+    struct Deep(Structure);
+    println!("Now {:?} will print!", Deep(Structure(7)));
+
+    #[derive(Debug)]
+    #[allow(dead_code)]
+    struct Person<'a> {
+        name: &'a str,
+        age: u8,
+    }
+
+    let name = "Peter";
+    let age = 27;
+    let peter = Person { name, age };
+    println!("{:#?}", peter);
+
+    // chapter 1.2.2
+    impl fmt::Display for Structure {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "{}", self.0)
+        }
+    }
+
+    #[derive(Debug)]
+    struct MinMax(i64, i64);
+
+    impl fmt::Display for MinMax {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "({},{})", self.0, self.1)
+        }
+    }
+
+    #[derive(Debug)]
+    struct Point2D {
+        x: f64,
+        y: f64,
+    }
+
+    impl fmt::Display for Point2D {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "x: {}, y:{}", self.x, self.y)
+        }
+    }
+
+    let min_max = MinMax(0, 14);
+    println!("Compare structures:");
+    println!("Display: {}", min_max);
+    println!("Debug: {:?}", min_max);
+
+    let big_range = MinMax(-300, 300);
+    let small_range = MinMax(-3, 3);
+    println!(
+        "The big range is {big} and the small is {small}",
+        small = small_range,
+        big = big_range
+    );
+
+    let point = Point2D { x: 3.3, y: 7.2 };
+
+    println!("Compare points:");
+    println!("Display: {}", point);
+    println!("Debug: {:?}", point);
+
+    // exercise
+    #[derive(Debug)]
+    struct Complex {
+        real: f64,
+        imag: f64,
+    }
+
+    impl fmt::Display for Complex {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "{} + {}i", self.real, self.imag)
+        }
+    }
+    let point = Complex {
+        real: 3.3,
+        imag: 7.2,
+    };
+
+    println!("Display: {}", point);
+    println!("Debug: {:?}", point);
+
+    // chapter 1.2.3
+    struct List(Vec<i32>);
+
+    impl fmt::Display for List {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            let vec = &self.0;
+            write!(f, "[")?;
+
+            for (count, v) in vec.iter().enumerate() {
+                if count != 0 {
+                    write!(f, ", ")?;
+                }
+                write!(f, "{}", v)?;
+            }
+
+            write!(f, "]")
+        }
+    }
+
+    let v = List(vec![1, 2, 3]);
+    println!("{}", v);
+
+    // exercise
+    struct ListOne(Vec<i32>);
+
+    impl fmt::Display for ListOne {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            let vec = &self.0;
+            write!(f, "[")?;
+
+            for (count, v) in vec.iter().enumerate() {
+                if count != 0 {
+                    write!(f, ", ")?;
+                }
+                write!(f, "{}:{}", count, v)?;
+            }
+
+            write!(f, "]")
+        }
+    }
+
+    let v = ListOne(vec![1, 2, 3]);
+    println!("{}", v);
+
+    // chapter 1.2.4
+    struct City {
+        name: &'static str,
+        lat: f32,
+        lon: f32,
+    }
+
+    impl fmt::Display for City {
+        fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+            let lan_c = if self.lat >= 0.0 { 'N' } else { 'S' };
+            let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
+
+            write!(
+                f,
+                "{}: {:.3}°{} {:.3}°{}",
+                self.name,
+                self.lat.abs(),
+                lan_c,
+                self.lon.abs(),
+                lon_c
+            )
+        }
+    }
+
+    #[derive(Debug)]
+    struct Color {
+        red: u8,
+        green: u8,
+        blue: u8,
+    }
+
+    impl fmt::Display for Color {
+        fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+            write!(
+                f,
+                "RGB ({}, {}, {}) 0x{:02X}{:02X}{:02X}",
+                self.red, self.green, self.blue, self.red, self.green, self.blue
+            )
+        }
+    }
+
+    for city in [
+        City {
+            name: "Dublin",
+            lat: 53.347778,
+            lon: -6.259722,
+        },
+        City {
+            name: "Oslo",
+            lat: 59.95,
+            lon: 10.75,
+        },
+        City {
+            name: "Vancouver",
+            lat: 49.25,
+            lon: -123.1,
+        },
+    ]
+    .iter()
+    {
+        println!("{}", *city);
+    }
+
+    for color in [
+        Color {
+            red: 128,
+            green: 255,
+            blue: 90,
+        },
+        Color {
+            red: 0,
+            green: 3,
+            blue: 254,
+        },
+        Color {
+            red: 0,
+            green: 0,
+            blue: 0,
+        },
+    ]
+    .iter()
+    {
+        println!("{}", *color)
+    }
 }
