6a9d4b95f97f93440aa900100d9931c69f33309c
ShengJieSang <shengj.sang@icloud.com>
2022-05-04 20:57:20 +0800

    feat: chapter 6.0

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,75 @@
 #![allow(dead_code)]
 
-fn main() {}
+use std::convert::From;
+use std::convert::TryFrom;
+use std::convert::TryInto;
+use std::fmt;
+use std::fmt::Formatter;
+
+#[derive(Debug)]
+struct Number {
+    value: i32,
+}
+
+impl From<i32> for Number {
+    fn from(value: i32) -> Number {
+        Number { value }
+    }
+}
+
+fn main() {
+    // chapter 6.1
+    let my_str = "hello";
+    let my_string = String::from(my_str);
+    println!("My string is {}", my_string);
+    let num = Number::from(30);
+    println!("My number is {:?}", num);
+
+    let int = 5;
+    let num: Number = int.into();
+    println!("My number is {:?}", num);
+
+    // chapter 6.2
+
+    #[derive(Debug, PartialEq)]
+    struct EvenNumber(i32);
+    impl TryFrom<i32> for EvenNumber {
+        type Error = ();
+
+        fn try_from(value: i32) -> Result<Self, Self::Error> {
+            if value % 2 == 0 {
+                Ok(EvenNumber(value))
+            } else {
+                Err(())
+            }
+        }
+    }
+
+    assert_eq!(EvenNumber::try_from(8), Ok(EvenNumber(8)));
+    assert_eq!(EvenNumber::try_from(5), Err(()));
+
+    let result: Result<EvenNumber, ()> = 8i32.try_into();
+    assert_eq!(result, Ok(EvenNumber(8)));
+    let result: Result<EvenNumber, ()> = 5i32.try_into();
+    assert_eq!(result, Err(()));
+
+    // chapter 6.3
+
+    struct Circle {
+        radius: i32,
+    }
+
+    impl fmt::Display for Circle {
+        fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+            write!(f, "Circle of radius {}", self.radius)
+        }
+    }
+
+    let circle = Circle { radius: 6 };
+    println!("{}", circle.to_string());
+
+    let parsed: i32 = "5".parse().unwrap();
+    let turbo_parsed = "10".parse::<i32>().unwrap();
+    let sum = parsed + turbo_parsed;
+    println!("Sum: {}", sum);
+}
