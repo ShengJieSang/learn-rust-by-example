745baf302bfffc04d521ef4c81d793b84b0fd96c
ShengJieSang <shengj.sang@icloud.com>
2022-05-05 10:47:11 +0800

    feat: chapter 8.0

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,247 @@
-#![allow(dead_code)]
+#![allow(dead_code, unreachable_code)]
 
-fn main() {}
+fn main() {
+    // chapter 8.1
+
+    let n = 5;
+
+    if n < 0 {
+        print!("{} is negative", n);
+    } else if n > 0 {
+        print!("{} is positive", n);
+    } else {
+        print!("{} is zero", n);
+    }
+
+    let big_n = if n < 10 && n > -10 {
+        println!(". and is small number, increase ten-fold");
+        10 * n
+    } else {
+        println!(". and is a big number, half the number");
+        n / 2
+    };
+
+    println!("{} -> {}", n, big_n);
+
+    // chapter 8.2
+    let mut count = 0u32;
+    loop {
+        count += 1;
+        if count == 3 {
+            println!("three");
+            continue;
+        }
+
+        println!("{}", count);
+
+        if count == 5 {
+            println!("Ok, that's enough");
+            break;
+        }
+    }
+
+    'outer: loop {
+        println!("Entered the outer loop");
+
+        'inner: loop {
+            println!("Entered the inner loop");
+            break 'outer;
+        }
+
+        println!("This point will never be reached");
+    }
+
+    println!("Exit the outer loop");
+
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    assert_eq!(result, 20);
+
+    // chapter 8.3
+    let mut n = 1;
+
+    while n < 101 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+
+        n += 1;
+    }
+
+    // chapter 8.4
+    for n in 1..101 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+    }
+
+    for n in 1..=100 {
+        if n % 15 == 0 {
+            println!("fizzbuzz");
+        } else if n % 3 == 0 {
+            println!("fizz");
+        } else if n % 5 == 0 {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+    }
+
+    let names = vec!["Bob", "Frank", "Ferris"];
+
+    for name in names.iter() {
+        match name {
+            &"Ferris" => {
+                println!("There is a rustacean among us!");
+            }
+            _ => println!("Hello {}", name),
+        }
+    }
+
+    let mut names = vec!["Bob", "Frank", "Ferris"];
+
+    for name in names.iter_mut() {
+        *name = match name {
+            &mut "Ferris" => "There is a rustacean among us!",
+            _ => "Hello",
+        }
+    }
+
+    println!("names: {:?}", names);
+
+    let boolean = true;
+
+    let binary = match boolean {
+        false => 0,
+        true => 1,
+    };
+
+    println!("{} -> {}", boolean, binary);
+
+    let reference = &4;
+
+    match reference {
+        &val => {
+            println!("Got a value via destructuring: {:?}", val);
+        }
+    }
+
+    match *reference {
+        val => {
+            println!("Got a value via dereferencing: {:?}", val);
+        }
+    }
+
+    let ref _is_a_reference = 3;
+
+    let value = 5;
+    let mut mut_value = 5;
+
+    match value {
+        ref r => println!("Got a reference to a value: {:?}", r),
+    }
+
+    match mut_value {
+        ref mut m => {
+            *m += 10;
+            println!("We added 10. `mut_value`: {:?}", m);
+        }
+    }
+
+    struct Foo {
+        x: (u32, u32),
+        y: u32,
+    }
+
+    let foo = Foo { x: (1, 2), y: 3 };
+    let Foo { x: (a, b), y: c } = foo;
+    println!("a = {}, b = {}, c = {}", a, b, c);
+
+    let Foo { y: i, x: j } = foo;
+    println!("i = {:?}, j = {:?}", i, j);
+
+    let pair = (2, -2);
+    println!("Tell me about {:?}", pair);
+    match pair {
+        (x, y) if x == y => println!("These are twins"),
+        (x, y) if x + y == 0 => println!("Antimatter,kaboom!"),
+        _ => println!("No correlation.."),
+    }
+
+    fn age() -> u32 {
+        15
+    }
+
+    println!("Tell me what type of person you are");
+
+    match age() {
+        0 => println!("I haven't celebrated my first birthday yet"),
+        n @ 1..=12 => println!("I'm a child of age {:?}", n),
+        n @ 13..=19 => println!("I'm a teen of age {:?}", n),
+        _ => println!("I'm an old person of age {:?}", n),
+    }
+
+    fn some_number() -> Option<u32> {
+        Some(42)
+    }
+
+    match some_number() {
+        Some(n @ 42) => println!("The answer is {:?}", n),
+        Some(n) => println!("Not interesting... {}", n),
+        _ => (),
+    }
+
+    // chapter 8.6
+    let number = Some(7);
+    let letter: Option<i32> = None;
+    let emoticon: Option<i32> = None;
+
+    if let Some(i) = number {
+        println!("Match {:?}", i);
+    }
+
+    if let Some(i) = letter {
+        println!("Match {:?}", i);
+    } else {
+        println!("Didn't match a number. let's go with a letter");
+    }
+
+    let i_like_letters = false;
+
+    if let Some(i) = emoticon {
+        println!("Match {:?}", i);
+    } else if i_like_letters {
+        println!("Didn't match a number. let's go with a letter");
+    } else {
+        println!("I don't like letters. Let's go with an emoticon :) !");
+    }
+
+    let mut optional = Some(0);
+    while let Some(i) = optional {
+        if i > 9 {
+            println!("Greater than 9, quit!");
+            optional = None;
+        } else {
+            println!("`i` is `{:?}`. Try again", i);
+            optional = Some(i + 1);
+        }
+    }
+}
