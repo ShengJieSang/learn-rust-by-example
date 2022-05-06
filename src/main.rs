c86bd74cdff40a3c0918c824ffc1969936f9f993
ShengJieSang <shengj.sang@icloud.com>
2022-05-06 10:07:52 +0800

    feat: chapter 10.0

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,99 @@
 #![allow(dead_code)]
 
-fn main() {}
+mod my_mod {
+    fn private_function() {
+        println!("called `my_mod::private_function()`");
+    }
+
+    pub fn function() {
+        println!("called `my_mod::function()`");
+        private_function();
+    }
+
+    pub mod nested {
+        pub fn function() {
+            println!("called `my_mod::nested::function()`");
+        }
+
+        #[allow(dead_code)]
+        fn private_function() {
+            println!("called `my_mod::nested::private_function()`");
+        }
+
+        // 使用 `pub(in path)` 语法定义的函数只在给定的路径中可见。
+        // `path` 必须是父模块（parent module）或祖先模块（ancestor module）
+        pub(in crate::my_mod) fn public_function_in_my_mod() {
+            print!("called `my_mod::nested::public_function_in_my_mod()`, that\n > ");
+            public_function_in_nested()
+        }
+
+        // 使用 `pub(self)` 语法定义的函数则只在当前模块中可见。
+        pub(self) fn public_function_in_nested() {
+            println!("called `my_mod::nested::public_function_in_nested");
+        }
+
+        // 使用 `pub(super)` 语法定义的函数只在父模块中可见。
+        pub(super) fn public_function_in_super_mod() {
+            println!("called my_mod::nested::public_function_in_super_mod");
+        }
+    }
+
+    pub fn call_public_function_in_my_mod() {
+        print!("called `my_mod::call_public_funcion_in_my_mod()`, that\n> ");
+        nested::public_function_in_my_mod();
+        print!("> ");
+        nested::public_function_in_super_mod();
+    }
+
+    // `pub(crate)` 使得函数只在当前 crate 中可见
+    pub(crate) fn public_function_in_crate() {
+        println!("called `my_mod::public_function_in_crate()");
+    }
+
+    // 嵌套模块的可见性遵循相同的规则
+    mod private_nested {
+        #[allow(dead_code)]
+        pub fn function() {
+            println!("called `my_mod::private_nested::function()`");
+        }
+    }
+}
+
+fn function() {
+    println!("called `function()`");
+}
+
+mod my {
+    pub struct OpenBox<T> {
+        pub contents: T,
+    }
+
+    #[allow(dead_code)]
+    pub struct CloseBox<T> {
+        contents: T,
+    }
+
+    impl<T> CloseBox<T> {
+        pub fn new(contents: T) -> CloseBox<T> {
+            CloseBox { contents }
+        }
+    }
+}
+fn main() {
+    function();
+
+    my_mod::function();
+    my_mod::nested::function();
+    my_mod::call_public_function_in_my_mod();
+
+    my_mod::public_function_in_crate();
+
+    let open_box = my::OpenBox {
+        contents: "public information",
+    };
+
+    println!("The open box contains: {}", open_box.contents);
+
+    let close_box = my::CloseBox::new("classified information");
+
+}
