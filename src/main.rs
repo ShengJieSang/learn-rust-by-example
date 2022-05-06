3775d91331bb87a3c97fd1bf5297aefe8740d8d3
ShengJieSang <shengj.sang@icloud.com>
2022-05-06 10:17:06 +0800

    feat: chapter 11.0

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 #![allow(dead_code)]
 
-fn main() {}
+// rustc main.rs --extern rary=library.rlib --edition=2018 && ./main
+fn main() {
+    rary::public_function();
+
+    // 报错！ `private_function` 是私有的
+    //rary::private_function();
+
+    rary::indirect_access();
+}
